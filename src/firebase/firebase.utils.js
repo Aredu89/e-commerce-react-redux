7ded24b58fddb076313d883dcb899ad57d4fbf92
Aredu89 <aredu_r89@hotmail.com>
2021-07-11 18:59:51 -0300

    implement firebase firestore to store authenticated user

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -9,7 +9,34 @@ const config = {
   storageBucket: "e-commerce-react-redux-def61.appspot.com",
   messagingSenderId: "258701909032",
   appId: "1:258701909032:web:1f5e67ddca5400c3afa91d",
-  measurementId: "G-J3F5B4RW3X"
+  measurementId: "G-J3F5B4RW3X",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch(error) {
+      console.log("Error creating user ", error.message)
+    };
+  };
+
+  return userRef;
+
 };
 
 firebase.initializeApp(config);
@@ -20,4 +47,4 @@ export const firestore = firebase.firestore();
 const provider = new firebase.auth.GoogleAuthProvider();
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-export default firebase;
+export default firebase
