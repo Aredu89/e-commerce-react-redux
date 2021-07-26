7b999d5e0c2578e5f3851b0b963cefb047aa2886
Aredu89 <aredu_r89@hotmail.com>
2021-07-26 17:33:02 -0300

    store shop data and collections in redux, create collections overview component and persist redux store

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,31 +1,13 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
+
+import CollectionsOverviewComponent from '../../components/collections-overview/collections-overview.component';
 
 import './shop.styles.scss';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-class ShopPage extends React.Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    }
-  }
-
-  render() {
-    const { collections } = this.state;
-    return(
-      <div className='shop-page'>
-        {
-          collections.map(({id, ...collection}) =>(
-            <CollectionPreview key={id} {...collection} />
-          ))
-        }
-      </div>
-    )
-  }
-}
+const ShopPage = () => (
+  <div className='shop-page'>
+    <CollectionsOverviewComponent />
+  </div>
+);
 
 export default ShopPage;
