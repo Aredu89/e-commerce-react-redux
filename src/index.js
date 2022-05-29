0033a9c45bc1594028b11906423162570933322d
Aredu89 <aredu_r89@hotmail.com>
2022-05-29 15:36:16 -0500

    adding redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -12,15 +14,17 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <BrowserRouter>
-    <UserProvider>
-      <CategoriesProvider>
-        <CartProvider>
-          <React.StrictMode>
-              <App />
-          </React.StrictMode>
-        </CartProvider>
-      </CategoriesProvider>
-    </UserProvider>
+    <Provider store={store}>
+      <UserProvider>
+        <CategoriesProvider>
+          <CartProvider>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+          </CartProvider>
+        </CategoriesProvider>
+      </UserProvider>
+    </Provider>
   </BrowserRouter>
 );
 
